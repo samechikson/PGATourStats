742044eeb1d2d7d6d1acf1d213982d7430420a4a
Sam Echikson <samuel.echikson@gmail.com>
2015-12-06 13:22:21 -0500

    Re-link main graph.js controller to the main project. Was called something else before.

---
+++ app/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module("pgaTourApp", []);
+
+angular.module.controller("mainController", function($scope){
+	
+
+})
+
+angular.module.directive("graph", function(){
+
+	return {
+		restrict: "EA",
+		template: "<div id='graph'></div>"
+	}
+
+});
